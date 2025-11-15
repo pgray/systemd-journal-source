e6ae0c3bf7df1b570560344f7cb8dc2eb58f3375
pgray <pgray@file.photo>
2025-11-15 13:10:51 -0800

    optional debug and config docs

--- src/config.rs
+++ src/config.rs
@@ -6,12 +6,16 @@ use fluvio_connector_common::connector;
 pub(crate) struct CustomConfig {
     pub current_user: bool,
     pub system: bool,
+    // only include entries from this host
     pub local_only: bool,
+    // only include entries from this boot
     pub runtime_only: bool,
+    // TODO: config variable to make tags filter records
+    // e.g. drop entry unless it has all tags?
     pub tags: Vec<String>,
     pub units: Vec<String>,
     pub flush_batch_size: u64,
     pub unit_types: Option<Vec<String>>,
     pub serialization_format: Option<String>,
-    pub debug: bool,
+    pub debug: Option<bool>,
 }
